a14b1eeac492e68dc92027620bbf23ffe3d41ea3
AlexanderPron <alexpronin85@gmail.com>
2021-01-27 17:14:12 +0300

    remake templates and pagination for pets pages

--- shelter/pets/views.py
+++ shelter/pets/views.py
@@ -5,50 +5,48 @@ from .models import Pet, ShelteredPets, Client, Photo
 from django.views import generic
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def index(request):
-  template = loader.get_template('index.html')
-  lastPets = Pet.objects.order_by('-enter_date')[:4] # Выбираем последних 4-х питомцев, поступивших в приют
-  allPets = Pet.objects.all()
-  paginator = Paginator(allPets, 6)
+def getPaginationData(request, dataQuerySet, countItemsOnPage):
+  paginator = Paginator(dataQuerySet, countItemsOnPage)
   numPages = paginator.num_pages
   numPageList = [i for i in range(1, numPages+1)]
   page = request.GET.get('page')
   try:
-    paginationPets = paginator.page(page)
+    paginationItems = paginator.page(page)
   except PageNotAnInteger:
-    paginationPets = paginator.page(1)
+    paginationItems = paginator.page(1)
   except EmptyPage:
-    paginationPets = paginator.page(paginator.num_pages)
+     paginationItems = paginator.page(paginator.num_pages)
   
-  pets = {
-      "lastPets": lastPets,
-      "paginationPets": paginationPets,
+  paginationData = {
+      "paginationItems": paginationItems,
       "numPageList": numPageList,
   }
+  return paginationData
+
+def index(request):
+  template = loader.get_template('index.html')
+  lastPets = Pet.objects.order_by('-enter_date')[:4] # Выбираем последних 4-х питомцев, поступивших в приют
+  allPets = Pet.objects.all()
+  pets = getPaginationData(request, allPets, 6)
+  pets["lastPets"] = lastPets
   return HttpResponse(template.render(pets, request))
 
 def cats_page(request):
   template = loader.get_template('cats.html')
   cats = Pet.objects.all().filter(pet_type = 'Cat')
-  cats_list = {
-      "cats": cats,
-  }
+  cats_list = getPaginationData(request, cats, 6)
   return HttpResponse(template.render(cats_list, request))
 
 def dogs_page(request):
   template = loader.get_template('dogs.html')
   dogs = Pet.objects.all().filter(pet_type = 'Dog')
-  dogs_list = {
-      "dogs": dogs,
-  }
+  dogs_list = getPaginationData(request, dogs, 6)
   return HttpResponse(template.render(dogs_list, request))
 
 def parrots_page(request):
   template = loader.get_template('parrots.html')
   parrots = Pet.objects.all().filter(pet_type = 'Parrot')
-  parrots_list = {
-      "parrots": parrots,
-  }
+  parrots_list = getPaginationData(request, parrots, 6)
   return HttpResponse(template.render(parrots_list, request))
 
 def sheltered_page(request):
